6fb43f59e3b2102e5bda6384aca6e82a8bf05a1e
kwqai <kwq2017@gmail.comgit config --global user.email kwq2017@gmail.com>
2024-03-27 01:43:36 -0700

    added welcome console.log line

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 
+console.log("Welcome to KWQAI Number Guessing Game");
+
 const answer = await inquirer.prompt([
     {
     name: "userGuessednumber",
